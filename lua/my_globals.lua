976191082bf69a6b7ecae68bece69c81ae8c9aad
Tummetott <19708025+Tummetott@users.noreply.github.com>
2022-04-10 17:57:19 +0200

    use vim.opt instead of vim.o. Removed all the stupid vim-ism

--- lua/my_globals.lua
+++ lua/my_globals.lua
@@ -1,112 +1,105 @@
+local autocmd = vim.api.nvim_create_autocmd
+
 -- Display line numbers
-vim.wo.number = true
+vim.opt.number = true
 
 -- Use relative numbers
-vim.wo.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Show signs inside the number column
-vim.wo.signcolumn = 'number'
+vim.opt.signcolumn = 'number'
 
 -- Only show tabline if there are at least two tab pages
-vim.o.showtabline = 1
+vim.opt.showtabline = 1
 
 -- Disable startup message
-vim.o.shortmess = vim.o.shortmess .. 'I'
+vim.opt.shortmess:append('I')
+
+-- When writing a file, show verbose output for number of lines / bytes written
+vim.opt.shortmess:remove('l')
 
 -- Only show one global statusline, independent of the number of windows
-vim.o.laststatus = 3
-
--- Don't display filenames as the statusline plugin already shows that
-vim.o.shortmess = vim.o.shortmess .. 'F'
+vim.opt.laststatus = 3
 
 -- Don't display mode changes as the statusline plugin already shows that
-vim.o.showmode = false
+vim.opt.showmode = false
 
--- Don't show line and column number since my statusline already shows this
-vim.o.ruler = false
+-- With a modeline command, you can e.g. define the filetype of a textfile
+-- explicitly)
+vim.opt.modeline = true
 
--- Enable modeline (with a modeline command, you can e.g. define the filetype
--- of a textfile explicitly)
-vim.o.modeline = true
-
--- Enable mouse support. This let's you scroll, jump, select, resize windows
--- and much more stuff. Also don't copy line numbers with the mouse
-vim.o.mouse = 'a'
+-- Enable mouse support for all modes. This let's you scroll, jump, select,
+-- resize windows etc.
+vim.opt.mouse = 'a'
 
 -- Enable true colors
-vim.o.termguicolors = true
+vim.opt.termguicolors = true
 
 -- Case insensitive search, except when using capital letters
-vim.o.smartcase = true
-vim.o.ignorecase = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- Don't highlight all my matches
-vim.o.hlsearch = false
-
--- Highlight all matches while tying a regex in ex-mode
-vim.o.inccommand = 'nosplit'
+vim.opt.hlsearch = false
 
 -- Always leave some lines below and above the cursor when scrolling
-vim.o.scrolloff = 5
-
--- Hide buffers instead of closing them. This means that you can have unwritten
--- changes to a buffer and open a new buffer, without being forced to write or
--- undo your changes first
-vim.o.hidden = true
+vim.opt.scrolloff = 5
 
 -- Disable swap files
-vim.o.swapfile = false
+vim.opt.swapfile = false
 
 -- Make undo persistent, even after restart
--- vim.o.undofile = true
-vim.o.undofile = true
+vim.opt.undofile = true
 
 -- With how many whitespace is a '\t' char displayed
-vim.o.tabstop = 4
+vim.opt.tabstop = 4
 
 -- How many whitespaces has a level of indentation (used in shifts, auto
 -- formatting). If set to zero, it will default to the value of tabstop
-vim.o.shiftwidth = 0
+vim.opt.shiftwidth = 0
 
 -- Expand tabs to spaces
-vim.o.expandtab = true
+vim.opt.expandtab = true
 
--- Indentation is dependending on the programming language
-vim.o.smartindent = true
+-- Indentation is dependending on the programming language. This is probably
+-- overwritten by treesitter
+vim.opt.smartindent = true
 
 -- New splits will be right and below instead of left and above
-vim.o.splitbelow = true
-vim.o.splitright = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
--- Diffsplits are always shown in a vertical split and foldcolumn is always one
--- character wide
-vim.o.diffopt = vim.o.diffopt .. ',vertical,foldcolumn:2'
+-- Start diff mode with vertical splits
+vim.opt.diffopt:append('vertical')
 
--- When doing 'cw' on a word, also delete the whitespace (Remove '_' option
--- from cpoptions)
-vim.o.cpoptions = string.gsub(vim.o.cpoptions, '_', '')
+-- When doing 'cw' on a word, also delete the whitespace after the word
+vim.opt.cpoptions:remove('_')
 
--- Hide the last entered ex command
-vim.cmd('autocmd CmdlineLeave * echom "" ')
+-- The fill character of folds is a whitespace. TODO: do I also want a
+-- whitespace for deleted lines in diffs?
+vim.opt.fillchars = { fold = ' ', diff = ' ' }
 
--- Hide line numbers in terminal mode
-vim.cmd('autocmd TermOpen * setlocal nonumber norelativenumber')
-
--- Automatically go in insert mode then opening a terminal
-vim.cmd('autocmd TermOpen * startinsert')
-
--- The fill character of folds is a whitespace
-vim.o.fillchars = "fold: ,diff: "
-
--- Set chars for blanks. You can enable them with 'set list'
-vim.o.listchars = 'space:⋅,tab:——▸,eol:↴'
+-- Set characters for blanks. Enable them with 'set list'
+vim.opt.listchars = { space = '⋅', tab = '——▸', eol = '↴' }
 
 -- Show foldcolumn when there are active folds. Show maximal 4 folds, then
 -- display it in numbers
-vim.wo.foldcolumn = "auto:4"
+vim.opt.foldcolumn = 'auto:4'
 
--- Only safe folds and cursor position when executing :mkview
-vim.o.viewoptions = "cursor,folds"
+-- Don't save the current directory when saving a view with :mkview
+vim.opt.viewoptions:remove('curdir')
 
--- Convenience function to print lua tables
+-- Hide the last entered ex command
+autocmd('CmdlineLeave', { command = 'echo ""' })
+
+-- Hide line numbers in terminal mode and start in insert mode
+autocmd('TermOpen', {
+    callback = function()
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
+        vim.api.nvim_command('startinsert')
+    end
+})
+
+-- Convenience function to print lua tables. Used for debugging
 P = vim.pretty_print
